5cc50bbddf1e8443c39b267bb4175c22ddca8bc4
Alan Cobos <imacretina5k@Alans-iMac.local>
2022-12-28 18:50:31 -0800

    Everything

--- index.js
+++ index.js
@@ -3,3 +3,19 @@ let monthlyExpenses = 0
 let annualExpenses = 0
 
 // Your Code Here
+for (let i = 0; i < weeklyExpenseQuestions.length; i++){
+    let stringAnswer = window.prompt(weeklyExpenseQuestions[i])
+    let numberAnswer = parseFloat(stringAnswer)
+    console.log("numberAnswer", numberAnswer, typeof numberAnswer)
+    weeklyExpenses = weeklyExpenses + numberAnswer
+    console.log("weeklyExpenses", weeklyExpenses)
+}
+
+
+//Monthly Expenses
+for (let i = 0; i < monthlyExpenseQuestions.length; i++){
+    let stringAnswer = window.prompt(monthlyExpenseQuestions[i])
+    let numberAnswer = parseFloat(stringAnswer)
+    monthlyExpenses = monthlyExpenses + numberAnswer
+    console.log("monthlyExpenses", monthlyExpenses)
+}
